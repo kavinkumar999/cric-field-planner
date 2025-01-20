486442a0af423bee71694d9ef002e589cc9e5581
Kavin Kumar <kavinkumarnkm007@gmail.com>
2025-01-20 22:08:36 +0530

    feat: initial the app 🚀

---
+++ src/app/components/header.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import React, { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes';
+import { Sun, Moon } from 'lucide-react';
+
+const ThemeButton = () => {
+  const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <ButtonSkeleton />;
+  }
+
+  return (
+    <button 
+      className="border p-2.5 rounded-lg text-foreground/60 transition-colors duration-200 ease-in-out"
+      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+      aria-label={resolvedTheme === 'dark' ? 'Switch to light mode' : 'Switch to dark mode'}
+    >
+      {resolvedTheme === 'dark' ? (
+        <Moon className="w-4 h-4" />
+      ) : (
+        <Sun className="w-4 h-4" />
+      )}
+    </button>
+  );
+};
+
+const ButtonSkeleton = () => (
+  <div className="h-9 w-9 rounded-lg border p-2.5">
+    <div className="animate-pulse h-4 w-4 bg-gray-200 dark:bg-gray-800 rounded" />
+  </div>
+);
+
+function Header() {
+  return (
+    <div className='w-full h-16'>
+      <div className='flex justify-end items-center h-full px-10 mt-5'>
+        <div className='flex items-end gap-2'>
+          <ThemeButton />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
